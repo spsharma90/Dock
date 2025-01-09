e7160fc641f26423c4bc84fc0825583482bb336c
Shubham <Shubhamse90@gmail.com>
2025-01-09 12:27:28 +0530

    Added a contact view

--- home/views.py
+++ home/views.py
@@ -6,4 +6,7 @@ def home(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')   
+    return render(request, 'about.html') 
+
+def contact(request):
+    return render(request, 'contact.html')   
